108b982f722bc8f7f759209ab0ded7301d5c8921
liliaim <gouxieai@gmail.com>
2023-08-14 13:27:05 +0900

    turbとgon修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,7 +20,8 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :index, status: :unprocessable_entity
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+      render :index, locals:{item_id: @item.id}, status: :unprocessable_entity
     end
   end
 
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,7 @@
 const pay = () => {
     const itemId = document.getElementById('item-id').value;
-
-  const publicKey = gon.public_key 
-   const payjp = Payjp(publicKey) 
+    const publicKey = gon.public_key 
+    const payjp = Payjp(publicKey) 
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -15,13 +14,14 @@ const pay = () => {
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
         payjp.createToken(numberElement).then(function (response) {
-      if (response.error) {
-      
+        const renderDom = document.getElementById("charge-form");
+        const itemIdObj = `<input value=${itemId} name='item_id' type="hidden">`;
+          if (response.error) {
+        renderDom.insertAdjacentHTML("beforeend", itemIdObj);
+    
       } else {
         const token = response.id;
-        const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        const itemIdObj = `<input value=${itemId} name='item_id' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
         renderDom.insertAdjacentHTML("beforeend", itemIdObj);
       }
@@ -31,9 +31,9 @@ const pay = () => {
       document.getElementById("charge-form").submit();
 
     });
-    
+
     e.preventDefault();
   });
 };
 
-window.addEventListener("load", pay);
+window.addEventListener("turbo:load", pay);
